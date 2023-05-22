18258c872752bdfdd65bc1ad10d9d62004833cc2
Newton Ombese <newtonombese1@gmail.com>
2023-05-22 17:59:28 +0300

    :art:feat: Adding my convertHTML code

---
+++ Freecodecamp/convertHTML.js
@@ -0,0 +1,13 @@
+function convertHTML(str) {
+    const htmlEntities = {
+      '&': '&amp;',
+      '<': '&lt;',
+      '>': '&gt;',
+      '"': '&quot;',
+      "'": '&apos;'
+    };
+    return str.replace(/[&<>"']/g, (match) => htmlEntities[match]);
+  }
+  
+  
+  convertHTML("Dolce & Gabbana");
